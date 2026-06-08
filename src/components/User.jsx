c2348e987a64db1b559598fedfe93b6d132ee87c
Kritika Bhagwat <kritikabhagwat@Kritikas-Air.lan>
2026-06-08 22:45:42 +0530

    user

---
+++ src/components/User.jsx
@@ -0,0 +1,10 @@
+const User = () =>{
+    return(
+        <div>
+            <h2>User Component</h2>
+            <p>This is the user component.</p>
+        </div>
+        
+    )
+}
+export default User;
