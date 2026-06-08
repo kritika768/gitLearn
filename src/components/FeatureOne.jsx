d1dfcbcca7c12ac7ae52a7961b018a50d148523f
Kritika Bhagwat <kritikabhagwat@Kritikas-Air.lan>
2026-06-08 22:32:27 +0530

    product

---
+++ src/components/FeatureOne.jsx
@@ -0,0 +1,10 @@
+const FeatureOne = () => {
+    return (
+        <div className="feature-one">
+            <h2>Feature One</h2>
+            <p>This is the first feature component.</p>
+        </div>
+    );
+};
+
+export default FeatureOne;
---
+++ src/components/Product.jsx
@@ -0,0 +1,10 @@
+const Product = () =>{
+    return(
+        <div>
+            <h2>Product Component</h2>
+            <p>This is the product component.</p>
+        </div>
+
+    )
+}
+export default Product;
