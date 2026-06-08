20e88be91c475f243f682492795e6b9f12629acb
Kritika Bhagwat <kritikabhagwat@Kritikas-Air.lan>
2026-06-08 22:15:54 +0530

    feature1

---
+++ src/components/Feature1.jsx
@@ -0,0 +1,9 @@
+const Feature1 = () =>{
+    return(
+        <div>
+            <h2>Feature 1</h2>
+            <p>This is the first feature.</p>
+        </div>
+    )
+}
+export default Feature1;
